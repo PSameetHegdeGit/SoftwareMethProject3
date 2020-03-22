31f8336159af18013975533171b5ca51fb621203
PSameetHegdeGit <sameethegde123@gmail.com>
2020-03-22 18:12:42 -0400

    ...

---
+++ src/application/Controller.java
@@ -0,0 +1,16 @@
+package application;
+
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+
+public class Controller {
+	
+	Button add;
+	
+	//calls add function from Tuition Manager 
+	public void clickAdd() {
+		System.out.println("Test add function");
+	}
+	
+	
+}
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,42 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
 
 
 public class Main extends Application {
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+			Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+			primaryStage.setTitle("GUI");
+			primaryStage.setScene(new Scene(root, 300, 275));
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public static void main(String[] args) {
-		launch(args);
-	}
+	
 }
+
+
+/*
+ * 
+ * 	BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+ * 
+*/
