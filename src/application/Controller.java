4c1719b1bc9514c0c9045e5f5bfd0b96e370baaa
PSameetHegdeGit <sameethegde123@gmail.com>
2020-03-23 17:40:53 -0400

    ...

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -7,7 +7,11 @@ import javafx.event.ActionEvent;
 
 public class Controller {
 	
+	TuitionManager tm = new TuitionManager();
+	
 	@FXML Button add;
+	@FXML Button remove;
+	@FXML Button print;
 	
 	@FXML private RadioButton instate; 
 	@FXML private RadioButton outstate;
@@ -17,39 +21,59 @@ public class Controller {
 	@FXML private CheckBox funding;
 	@FXML private CheckBox tristateStudent;
 	@FXML private CheckBox exchangeStudent;
-	//@FXML private TextField fundingAmount;
+	
+	@FXML private TextField fundingAmount;
 	
 	
+	//Initializes my text 
+	@FXML
+	public void initialize() {
+		System.out.println("Hello!");
+		fundingAmount.setDisable(true);
+	}
+		
+	//Disables checkboxes
 	@FXML
 	public void clickRadioButton(ActionEvent event) {
 		if(instate.isSelected()) {
 			System.out.println("Instate Selected");
 			funding.setDisable(false);
-			//fundingAmount.setDisable(false);
+			fundingAmount.setDisable(false);
 			tristateStudent.setDisable(true);
 			exchangeStudent.setDisable(true);
 		}
 		if(outstate.isSelected()) {
 			System.out.println("Outstate Selected");
-			//fundingAmount.setDisable(true);
+			fundingAmount.setDisable(true);
 			funding.setDisable(true);
 			tristateStudent.setDisable(false);
 			exchangeStudent.setDisable(true);
 		}
 		if(international.isSelected()) {
 			System.out.println("International Selected");
-			//fundingAmount.setDisable(false);
+			fundingAmount.setDisable(true);
 			funding.setDisable(true);
 			tristateStudent.setDisable(true);
 			exchangeStudent.setDisable(false);
 		}
+		
 	}
 	
-	//calls add function from Tuition Manager 
+	//handles each function
 	@FXML
-	public void clickAdd() {
+	public void clickAdd(ActionEvent event) {
 		System.out.println("Test add function");
+		System.out.println(fundingAmount.getText());
 	}
+	
+	public void clickRemove(ActionEvent event) {
+		System.out.println("Test remove function");
+		
+	}
+	public void clickPrint(ActionEvent event) {
+		System.out.println("Test print function");
+	}
+	
 
 	
 	
